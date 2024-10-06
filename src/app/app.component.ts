286aab0702081076f940351e1c6fb109289745b9
JamesAskelson <jamesaskelson630@gmail.com>
2024-10-05 19:27:15 -0500

    angular-dom-add-remove-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostListComponent } from "./post-list/post-list.component";
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostListComponent, NgComponentOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-ng-rendering';
+  constructor (private viewContainer: ViewContainerRef) {}
+
+  loadComponent(){
+    this.viewContainer.createComponent(PostListComponent)
+  }
+
+  removeComponent(){
+    this.viewContainer.remove()
+  }
 }
